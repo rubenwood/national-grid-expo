acd3ad10fe8fd77732e8e543847923b23b91c70c
N/A <ruben.wood1@gmail.com>
2026-05-05 20:39:30 +0100

    initial commit for nation grid - native prototype, inspired by https://grid.iamkate.com/

---
+++ app/components/generation.tsx
@@ -0,0 +1,150 @@
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { PieChart } from "react-native-gifted-charts";
+import {
+    CATEGORY_MAP,
+    COLOUR_MAP,
+    EnergySource,
+    GroupedEnergyData,
+} from "../models/generation-models";
+
+export function LoadingText() {
+  return (
+    <ThemedView>
+      <ThemedText>Loading...</ThemedText>
+    </ThemedView>
+  );
+}
+
+export function EnergySourceList(props: any) {
+  return (
+    <>
+      {props.data.map((item: EnergySource) => (
+        <ThemedText style={styles.sectionBody} key={`energy-${item.name}`}>
+          {item.value}% {item.name}
+        </ThemedText>
+      ))}
+    </>
+  );
+}
+
+export default function GenerationWheel() {
+  const [normalisedData, setNormalisedData] = useState<EnergySource[] | null>(
+    null,
+  );
+  const [chartData, setChartData] = useState<any[]>([]);
+  const [groupedData, setGroupedData] = useState<GroupedEnergyData>();
+
+  const fetchGenerationData = async () => {
+    const resp = await fetch("https://api.carbonintensity.org.uk/generation");
+    const respJson = await resp.json();
+
+    const normalData = normaliseData(respJson.data.generationmix);
+    setNormalisedData(normalData);
+
+    const inChartData = mapChartData(normalData);
+    setChartData(inChartData);
+
+    const inGroupedData = groupData(normalData);
+    setGroupedData(inGroupedData);
+  };
+
+  function normaliseData(data: any[]): EnergySource[] {
+    return data.map((item) => ({
+      name: item.fuel,
+      value: item.perc,
+      colour: COLOUR_MAP[item.fuel] ?? "#999",
+      category: CATEGORY_MAP[item.fuel] ?? "other",
+    }));
+  }
+
+  function mapChartData(data: any) {
+    const chartData = data.map((item: any) => ({
+      value: item.value,
+      color: COLOUR_MAP[item.name] ?? "#999",
+      text: `${item.name} (${item.value}%)`,
+    }));
+    return chartData;
+  }
+
+  function groupData(data: EnergySource[]): GroupedEnergyData {
+    const grouped: GroupedEnergyData = {
+      fossil: [],
+      fossil_total: 0,
+      renewable: [],
+      renewable_total: 0,
+      other: [],
+      other_total: 0,
+    };
+
+    for (const item of data) {
+      const category = item.category;
+
+      grouped[category].push(item);
+      grouped[`${category}_total`] += item.value;
+    }
+    return grouped;
+  }
+
+  useEffect(() => {
+    fetchGenerationData();
+  });
+
+  if (!groupedData) {
+    return;
+  }
+
+  return (
+    <ThemedView>
+      {/* <ThemedText>{JSON.stringify(groupedData)}</ThemedText> */}
+      <PieChart
+        data={chartData}
+        donut
+        showText
+        textColor="white"
+        radius={160}
+        innerRadius={0}
+        textSize={10}
+      />
+      <ThemedView>
+        <ThemedText style={[styles.sectionStyle, styles.fossil]}>
+          {groupedData.fossil_total}% Fossil Fuels
+        </ThemedText>
+        <EnergySourceList data={groupedData.fossil} />
+        <ThemedText style={[styles.sectionStyle, styles.renewable]}>
+          {groupedData.renewable_total}% Renewables
+        </ThemedText>
+        <EnergySourceList data={groupedData.renewable} />
+        <ThemedText style={[styles.sectionStyle, styles.other]}>
+          {groupedData.other_total}% Other
+        </ThemedText>
+        <EnergySourceList data={groupedData.other} />
+      </ThemedView>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionStyle: {
+    marginTop: 20,
+    padding: 16,
+    borderTopRightRadius: 12,
+    borderTopLeftRadius: 12,
+    backgroundColor: "#1c1c1e",
+  },
+  fossil: {
+    backgroundColor: COLOUR_MAP["gas"],
+  },
+  renewable: {
+    backgroundColor: COLOUR_MAP["wind"],
+  },
+  other: {
+    backgroundColor: COLOUR_MAP["nuclear"],
+  },
+  sectionBody: {
+    padding: 16,
+    backgroundColor: "#38383a",
+  },
+});
---
+++ app/models/generation-models.tsx
@@ -0,0 +1,45 @@
+export type EnergySource = {
+  name: string;
+  value: number; // %
+  colour: string;
+  category: "fossil" | "renewable" | "other";
+};
+
+export type GridData = {
+  demand: number;
+  generation: number;
+  sources: EnergySource[];
+};
+
+export type GroupedEnergyData = {
+  fossil: EnergySource[];
+  fossil_total: number;
+  renewable: EnergySource[];
+  renewable_total: number;
+  other: EnergySource[];
+  other_total: number;
+};
+
+export const CATEGORY_MAP: Record<string, EnergySource["category"]> = {
+  gas: "fossil",
+  coal: "fossil",
+  biomass: "renewable",
+  wind: "renewable",
+  solar: "renewable",
+  hydro: "renewable",
+  nuclear: "other",
+  imports: "other",
+  other: "other",
+};
+
+export const COLOUR_MAP: Record<string, string> = {
+  biomass: "#a78bfa",
+  coal: "#444",
+  imports: "#94a3b8",
+  gas: "#f87171",
+  nuclear: "#60a5fa",
+  other: "#9ca3af",
+  hydro: "#38bdf8",
+  solar: "#facc15",
+  wind: "#4ade80",
+};
